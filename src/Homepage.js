934c2b3b60d1825fbba48ce2567f24cb119efb00
John Patrick Salcedo <john@Johns-Mac-mini.local>
2019-05-10 20:28:32 +0800

    Add image and change Homepage name

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+class Homepage extends Component {
+  render () {
+    return (
+        <div>
+        </div>
+      );
+  }
+}
+
+export default Homepage;
