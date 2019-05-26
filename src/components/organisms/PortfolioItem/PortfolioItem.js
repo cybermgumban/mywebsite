d39e4ed9c6aa91cff7a95683e077cb43f0f92e0a
Marlon Gumban <mgumban@cybermgumban.com>
2019-05-26 16:45:52 +0800

    Complete Portfolio page

---
+++ src/components/organisms/PortfolioItem/PortfolioItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const OutsideWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    background-image: linear-gradient(to top, #dfe9f3 0%, white 100%);
+`
+
+const InsideWrapper = styled.div`
+    margin: 20px 0;
+`
+
+const ImageWrapper = styled.img`
+    margin: 20px;
+    border: 1px solid grey;
+`
+
+const PortfolioItem = (props) => {
+    return (
+        <OutsideWrapper>
+            <ImageWrapper src={props.imgSrc} alt="sample"/>
+            <InsideWrapper>
+                <p>Title: {props.title}</p>
+                <p>Type: {props.type}</p>
+                <p>Description: {props.description}</p>
+                <p>Modules: </p>
+                {props.modules.map((mod) => (
+                    <ul key={mod.index}>
+                        <li key={mod.index}>{mod}</li>
+                    </ul>
+                ))}
+            </InsideWrapper>
+        </OutsideWrapper>
+    )
+}
+
+export default PortfolioItem;
--- src/components/pages/PortfolioPage.js
+++ src/components/pages/PortfolioPage.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
 
+//components
+import PortfolioItem from '../organisms/PortfolioItem/PortfolioItem';
+
+const FIXTURES = {
+    input: [
+        {
+            image: "http://lorempixel.com/300/300",
+            title: "My website - www.cybermgumban.com",
+            type: "Web Application",
+            description: "A website that showcase my portfolio, resume and a way to contact me.",
+            modules: [ "React", "Styled-Components", "CSS Grid" ]
+        },
+        {
+            image: "http://lorempixel.com/300/300",
+            title: "Fitbook",
+            type: "Web Application",
+            description: "A social platform for people that are into fitness.",
+            modules: [ "React", "Styled-Components", "CSS Grid" ]
+        },
+
+        {
+            image: "http://lorempixel.com/300/300",
+            title: "Fitbook",
+            type: "Mobile Application",
+            description: "A mobile app social platform for people that are into fitness.",
+            modules: [ "React-Native", "Styled-Components", "CSS Grid" ]
+        }
+    ]
+}
+
 const PortfolioPageWrapper = styled.div`
-    height: 100%;
-    width: 100%;
-    background-image: linear-gradient(-225deg, #473B7B 0%, #3584A7 51%, #30D2BE 100%);
+    background-image: linear-gradient(to top, #dfe9f3 0%, white 100%);
+
+    & > h1 {
+        text-align: center;
+        margin-top: 20px;
+    }
 `
 
 const PortfolioPage = () => {
     return (
         <PortfolioPageWrapper>
-
+            <h1>Portfolio</h1>
+            {FIXTURES.input.map((inputs, index) => (
+                <PortfolioItem 
+                    key={index} 
+                    imgSrc={inputs.image} 
+                    title={inputs.title} 
+                    type={inputs.type} 
+                    description={inputs.description} 
+                    modules={inputs.modules}
+                />
+            ))}
         </PortfolioPageWrapper>
     )
 }
